e472f2c466890ad32355915f6e4872e87f4ca7ea
bo.liu04 <bo.liu04@uim-solution.com>
2026-08-27 09:43:58 +0800

    Initial Commit

---
+++ Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by bo.liu on 2026/8/27.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
